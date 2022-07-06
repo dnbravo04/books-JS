a6152f3f311cbd3023d5c198652689a9085dcc8b
Diego Bravo A <digobravo@outlook.com>
2022-07-06 12:14:01 -0500

    Designing Frontend

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devmode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: 'development',
+  entry: './Frontend/app.js',
+  output: {
+    path: path.join(__dirname, 'Backend/public'),
+    filename: 'js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          devmode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './Frontend/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/bundle.css',
+    }),
+  ],
+  devtool: 'source-map'
+};
